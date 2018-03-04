880d1258c7f4d3bcee39e74210d481b6f715dda3
robbieve <robbieeve751994@gmail.com>
2018-03-04 23:16:26 +0800

    Added funcationl search for nearby venue

--- src/staff/searchVenues.js
+++ src/staff/searchVenues.js
@@ -3,7 +3,7 @@ import NavBar from "../layouts/NavBar"
 
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
-import classNames from "classnames"
+import { Grid, Row, Col, Image } from "react-bootstrap"
 import API from "../services/api"
 
 const FontAwesome = require("react-fontawesome")
@@ -22,6 +22,20 @@ class SearchVenues extends Component {
         club: false,
         pub: false,
         restaurant: false
+      },
+      filterTypes: {
+        venue: true,
+        service: false
+      },
+      services: {
+        all: true,
+        alcohol: false,
+        cocktails: false,
+        drinks: false,
+        food: false,
+        breakfast: false,
+        lunch: false,
+        dinner: false
       },
       loading: true
     }
@@ -68,6 +82,49 @@ class SearchVenues extends Component {
     })
   }
 
+  handleServiceClick = event => {
+    const { name } = event.target
+    this.setState(prev => {
+      let services = prev.services
+      if (name === "all") {
+        services["all"] = !services["all"]
+        Object.keys(prev.services).forEach(filter => {
+          if (filter !== "all") {
+            services[filter] = services["all"]
+          }
+        })
+      } else {
+        services[name] = !services[name]
+        services["all"] = false
+      }
+
+      const venues = prev.defaultVenues.filter(venue => {
+        const servicesArray = Object.keys(services).filter(
+          type => services[type]
+        )
+        let isFiltered = false
+        venue.services.forEach(type => {
+          if (servicesArray.includes(type)) {
+            isFiltered = true
+          }
+        })
+        return isFiltered
+      })
+
+      return { services, venues }
+    })
+  }
+
+  handleFilterBy = event => {
+    this.setState(prev => {
+      const filters = prev.filterTypes
+      Object.keys(filters).forEach(type => {
+        filters[type] = !filters[type]
+      })
+      return { filterTypes: filters }
+    })
+  }
+
   renderVenueLists = () => {
     if (this.state.loading) {
       return <div>Loading...</div>
@@ -84,16 +141,78 @@ class SearchVenues extends Component {
           <p>{venue.type.map(type => type.capitalize()).join(" / ")}</p>
           <p>Monday - Friday: 10AM - 11PM</p>
           <p>Saturday - Sunday: 10AM - 11PM</p>
-          <p>Services: </p>
+
+          <Grid>
+            <Row>
+              Services:
+              {venue.services.map((service, index) => {
+                if (service && index < 3) {
+                  return (
+                    <Col key={index} md={2}>
+                      <Image
+                        src={require(`../assets/icons/venue/services/white/${service}.png`)}
+                        style={{
+                          width: "2.5em",
+                          height: "2.5em",
+                          borderRadius: "100%",
+                          padding: "2px",
+                          backgroundColor: "#5e5cbd"
+                        }}
+                      />
+                    </Col>
+                  )
+                }
+              })}
+            </Row>
+          </Grid>
         </div>
         <div className="col-sm-3 venue-action">
           <p>
-            <FontAwesome name="map-marker" />&nbsp;&nbsp;Sydney, CBD
+            <FontAwesome name="map-marker" />&nbsp;&nbsp;{venue.locationName}
           </p>
-          <button className="btn-round btn-dark">Interested</button>
+          <button className="btn-round btn-passive">Interested</button>
         </div>
       </div>
     ))
+  }
+
+  renderFilterButtons = () => {
+    if (this.state.filterTypes.venue) {
+      return Object.keys(this.state.venueTypeFilters).map((venue, index) => {
+        const wide = venue === "restaurant" ? "wide-md" : "wide-sm"
+        const active = this.state.venueTypeFilters[venue]
+          ? "btn-active"
+          : "btn-passive"
+        return (
+          <button
+            key={index}
+            active={venue.active}
+            name={venue}
+            className={`a-btn btn-round ${wide} ${active}`}
+            style={{ fontSize: "14px" }}
+            onClick={this.handleVenueTypeClick}
+          >
+            {venue.capitalize()}
+          </button>
+        )
+      })
+    }
+
+    return Object.keys(this.state.services).map((service, index) => {
+      const active = this.state.services[service] ? "btn-active" : "btn-passive"
+      return (
+        <button
+          key={index}
+          active={service.active}
+          name={service}
+          className={`a-btn btn-round "wide-sm" ${active}`}
+          style={{ fontSize: "14px" }}
+          onClick={this.handleServiceClick}
+        >
+          {service.capitalize()}
+        </button>
+      )
+    })
   }
 
   render() {
@@ -101,42 +220,34 @@ class SearchVenues extends Component {
       <div>
         <NavBar />
 
-        <div className="container xem cont-flex">
+        <div
+          className="xem cont-flex"
+          style={{ paddingLeft: "5%", paddingRight: "5%", height: "85vh" }}
+        >
           <div className="card card-md">
             <div className="card-header">
               <h4>NEARBY VENUE</h4>
               <div className="card-filter">
                 <div className="xxm">
                   <span>FILTER BY:&nbsp;&nbsp;</span>
-                  <button className="a-btn btn-round btn-dark">
-                    Type of Venue
-                  </button>
-                  <button className="a-btn btn-round btn-passive">
-                    Type of Service
-                  </button>
+                  {Object.keys(this.state.filterTypes).map((type, index) => {
+                    const active = this.state.filterTypes[type]
+                      ? "btn-dark"
+                      : "btn-passive"
+                    return (
+                      <button
+                        key={index}
+                        name={type}
+                        className={`a-btn btn-round  ${active}`}
+                        onClick={this.handleFilterBy}
+                      >
+                        {`Type of ${type.capitalize()}`}
+                      </button>
+                    )
+                  })}
                 </div>
                 <div className="xxm mini-container">
-                  {Object.keys(this.state.venueTypeFilters).map(
-                    (venue, index) => {
-                      const wide =
-                        venue === "restaurant" ? "wide-md" : "wide-sm"
-                      const active = this.state.venueTypeFilters[venue]
-                        ? "btn-active"
-                        : "btn-passive"
-                      return (
-                        <button
-                          key={index}
-                          active={venue.active}
-                          name={venue}
-                          className={`a-btn btn-round ${wide} ${active}`}
-                          style={{ fontSize: "14px" }}
-                          onClick={this.handleVenueTypeClick}
-                        >
-                          {venue.capitalize()}
-                        </button>
-                      )
-                    }
-                  )}
+                  {this.renderFilterButtons()}
                 </div>
               </div>
             </div>
@@ -145,7 +256,7 @@ class SearchVenues extends Component {
             </div>
           </div>
 
-          <div className="card card-sm">
+          <div className="card card-md">
             <div className="card-header">
               <h4>UPCOMING EVENTS NEAR YOU</h4>
               <div className="card-filter">
@@ -159,22 +270,40 @@ class SearchVenues extends Component {
                   </button>
                 </div>
                 <div className="xxm card-filter">
-                  <button className="a-btn btn-round wide-sm btn-passive">
+                  <button
+                    className="a-btn btn-round wide-sm btn-passive"
+                    style={{ fontSize: "14px" }}
+                  >
                     All
                   </button>
-                  <button className="a-btn btn-round wide-md btn-active">
+                  <button
+                    className="a-btn btn-round wide-sm btn-active"
+                    style={{ fontSize: "14px" }}
+                  >
                     Wedding
                   </button>
-                  <button className="a-btn btn-round wide-md btn-passive">
+                  <button
+                    className="a-btn btn-round wide-sm btn-passive"
+                    style={{ fontSize: "14px" }}
+                  >
                     Birthday
                   </button>
-                  <button className="a-btn btn-round wide-md btn-passive">
+                  <button
+                    className="a-btn btn-round wide-md btn-passive"
+                    style={{ fontSize: "14px" }}
+                  >
                     Conference
                   </button>
-                  <button className="a-btn btn-round wide-md btn-passive">
+                  <button
+                    className="a-btn btn-round wide-md btn-passive"
+                    style={{ fontSize: "14px" }}
+                  >
                     Music Festival
                   </button>
-                  <button className="a-btn btn-round wide-md btn-passive">
+                  <button
+                    className="a-btn btn-round wide-md btn-passive"
+                    style={{ fontSize: "14px" }}
+                  >
                     Family Events
                   </button>
                 </div>
