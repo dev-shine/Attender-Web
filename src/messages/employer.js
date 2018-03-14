70c1f99db6b4c0595168e2c95b5c58de4d842976
robbieve <robbieeve751994@gmail.com>
2018-03-14 11:41:20 +0800

    Updates for functional update on messaging

--- src/messages/employer.js
+++ src/messages/employer.js
@@ -5,57 +5,223 @@ import ".././styles/style.css"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 import TextTruncate from "react-text-truncate"
+import API from "./../services/api"
+import constant from "./../configs/constant"
+var moment = require("moment")
+const ws = require("adonis-websocket-client")
+const io = ws("http://localhost:3333")
+const client = io.channel("chat").connect()
 
 class EmployerMessage extends Component {
   constructor(props) {
     super(props)
+    this.inputMessageRef = null
+    this.messagesEnd = null
     this.state = {
-      threads: [1, 2, 3, 0, 2, 0, 0, 7, 8]
+      inputMessage: "",
+      renderContactsLoading: true,
+      renderMessagesLoading: true,
+      loading: true,
+      thread: {},
+      threads: [],
+      conversation: []
     }
   }
 
+  componentWillMount = async () => {
+    API.initRequest()
+
+    let profile = await API.getProfile()
+    this.setState({ profile })
+    API.get("staff-messages").then(res => {
+      if (res.status) {
+        this.setState(
+          {
+            threads: res.threads,
+            renderContactsLoading: false,
+            thread: res.threads[0]
+          },
+          () => {
+            this.getConversation()
+            this.connectSocket()
+          }
+        )
+      }
+    })
+  }
+
+  getStaffMessages = () => {
+    this.setState(
+      {
+        renderContactsLoading: false
+      },
+      () => {
+        API.get("staff-messages").then(res => {
+          if (res.status) {
+            this.setState({
+              threads: res.threads,
+              renderContactsLoading: false
+            })
+          }
+        })
+      }
+    )
+  }
+
+  onChangeInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleThreadClick = thread => {
+    if (this.state.thread._id) {
+      this.leaveSocketRoom(this.state.thread._id)
+    }
+
+    this.setState({ thread, renderMessagesLoading: true }, function() {
+      this.getConversation()
+      this.connectSocket()
+    })
+  }
+
+  getConversation = () => {
+    API.get(`conversation/${this.state.thread._id}`).then(res => {
+      if (res.status) {
+        let formatMessages = []
+        let date = null
+        res.messages.map((res, id) => {
+          if (!date) {
+            date = moment(res.sentAt).format("MM/DD/YYYY")
+          }
+
+          var $formatMessages = {
+            _id: id,
+            text: res.message,
+            createdAt: moment(res.sentAt).format("h:mm A"),
+            conversation: res.conversation,
+            user: {
+              _id: res.sender,
+              name: "",
+              avatar: res.staff.avatar
+            }
+          }
+
+          if (date !== moment(res.sentAt).format("MM/DD/YYYY")) {
+            $formatMessages.setDateBar = date
+          }
+
+          formatMessages.push($formatMessages)
+        })
+
+        this.setState({
+          conversation: formatMessages,
+          renderMessagesLoading: false
+        })
+        this.scrollToBottom()
+      }
+    })
+  }
+
+  connectSocket = () => {
+    var self = this
+    var threadId = this.state.thread._id
+
+    client.joinRoom(threadId, {}, (err, message) => {
+      console.log("join room", err, message)
+    })
+
+    client.on("message", function(room, message) {
+      console.log("room", room, "message", message)
+      if (message == "refresh-messages") {
+        self.getConversation()
+        self.getStaffMessages()
+      }
+    })
+  }
+
+  leaveSocketRoom = id => {
+    client.leaveRoom(id, {}, (err, message) => {
+      console.log(err, message)
+    })
+  }
+
+  scrollToBottom = () => {
+    this.messagesEnd.scrollIntoView()
+  }
+
+  onSend = event => {
+    event.preventDefault()
+    var self = this
+
+    var thread = this.state.thread
+
+    var body = {
+      receiver: thread.usid,
+      message: this.state.inputMessage,
+      venue: thread.uselect,
+      convo: this.state.thread._id
+    }
+
+    API.post("new-venue-message", body).then(res => {
+      if (res.status) {
+        self.setState({ inputMessage: "" }, function() {
+          self.inputMessageRef.focus()
+        })
+      }
+    })
+  }
+
   renderMessages = () => {
+    if (this.state.renderMessagesLoading) {
+      return (
+        <div className="container xem center navigator">
+          <img alt="" src={require("./../assets/icons/loading.svg")} />
+        </div>
+      )
+    }
+
     return (
       <div className="m-content">
-        <div className="m-message-left">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt e velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-        <div className="m-message-left">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        </div>
-        <div className="m-time-left">9:45 AM</div>
-
-        <div className="m-message-right">
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa{" "}
-        </div>
-        <div className="m-message-right">
-          quae ab illo inventore veritati it aut fugit, sed quia consequuntur
-          magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro
-          quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-          adipisc
-        </div>
-        <div className="m-time-right">10:00 AM</div>
-
-        <div className="m-line">
-          <div className="a-line" />
-          <div className="m-today">TODAY</div>
-        </div>
-
-        <div className="m-message-left">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        </div>
-        <div className="m-time-left">9:45 AM</div>
-
-        <div className="m-message-right">
-          quae ab illo inventore veritati it aut fugit, sed quia consequuntur
-          magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro
-          quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
-          adipisc
-        </div>
-        <div className="m-time-right">10:00 AM</div>
+        {this.state.conversation
+          .slice(0)
+          .reverse()
+          .map((message, index) => {
+            return (
+              <div key={index}>
+                {message.setDateBar ? (
+                  <div className="m-line">
+                    <div className="a-line" />
+                    <div className="m-today">{message.setDateBar}</div>
+                  </div>
+                ) : null}
+                <div
+                  className={
+                    this.state.profile._id === message.user._id
+                      ? "m-message-right"
+                      : "m-message-left"
+                  }
+                >
+                  {message.text}
+                </div>
+                <div
+                  className={
+                    this.state.profile._id === message.user._id
+                      ? "m-time-right"
+                      : "m-time-left"
+                  }
+                >
+                  {message.createdAt}
+                </div>
+              </div>
+            )
+          })}
+        <div
+          style={{ float: "left", clear: "both" }}
+          ref={el => {
+            this.messagesEnd = el
+          }}
+        />
       </div>
     )
   }
@@ -80,29 +246,37 @@ class EmployerMessage extends Component {
   }
 
   renderContacts = () => {
+    if (this.state.renderContactsLoading) {
+      return (
+        <div className="container xem center navigator">
+          <img alt="" src={require("./../assets/icons/loading.svg")} />
+        </div>
+      )
+    }
+
     return (
       <div>
         {this.state.threads.map((thread, index) => {
           return (
-            <div key={index} className="m-thread">
+            <div
+              key={index}
+              className="m-thread"
+              onClick={this.handleThreadClick.bind(this, thread)}
+            >
               <div className="row">
                 <div className="col-sm-3">
-                  <img
-                    alt=""
-                    className="profile-thumb"
-                    src="http://via.placeholder.com/150x150"
-                  />
+                  <img alt="" className="profile-thumb" src={thread.uavatar} />
                 </div>
                 <div className="col-sm-9">
-                  <span>Dummy Name</span>
+                  <span>{thread.uname}</span>
                   <div className="m-thread-msg">
-                    {thread ? (
-                      <span className="a-badge pull-right">{thread}</span>
+                    {!thread.seen ? (
+                      <span className="a-badge pull-right" />
                     ) : null}
                     <TextTruncate
                       line={1}
                       truncateText="…"
-                      text="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                      text={thread.message}
                     />
                   </div>
                 </div>
@@ -123,10 +297,21 @@ class EmployerMessage extends Component {
             src={require(".././assets/icons/messages/attachment.png")}
           />
         </a>
-        <input type="text" placeholder="Type here to write something" />
-        <button>
-          <i className="fa fa-paper-plane" />
-        </button>
+        <form onSubmit={this.onSend}>
+          <input
+            type="text"
+            name="inputMessage"
+            placeholder="Type here to write something"
+            ref={input => {
+              this.inputMessageRef = input
+            }}
+            value={this.state.inputMessage}
+            onChange={this.onChangeInput}
+          />
+          <button onClick={this.onSend}>
+            <i className="fa fa-paper-plane" />
+          </button>
+        </form>
       </div>
     )
   }
