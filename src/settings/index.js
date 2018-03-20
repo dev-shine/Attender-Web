97a054ff489d1b56c1f73f839619facd5cc55ac9
robbieve <robbieeve751994@gmail.com>
2018-03-20 22:10:26 +0800

    Functional update for email change in general settings.

--- src/settings/index.js
+++ src/settings/index.js
@@ -4,14 +4,56 @@ import ".././styles/global.css"
 import ".././styles/style.css"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
+import API from "./../services/api"
+import "./settings.css"
 
 class Settings extends Component {
   constructor(props) {
     super(props)
     this.state = {
       step: 1,
-      isLoading: false
+      isLoading: false,
+      isChangingEmail: false,
+      emailChangeMessage: "",
+      oldEmail: "",
+      newEmail: ""
     }
+  }
+
+  componentWillMount = async () => {
+    API.initRequest()
+  }
+
+  onChangeInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onSaveEmailChange = event => {
+    event.preventDefault()
+
+    const body = {
+      oldEmail: this.state.oldEmail,
+      newEmail: this.state.newEmail
+    }
+
+    // Should validate if it is a valid email and not empty
+    console.log("the body", body)
+
+    API.post("user/profile/change-email", body).then(res => {
+      if (!res.status) {
+        this.setState({ emailChangeMessage: res.message })
+      }
+
+      if (res.status) {
+        this.setState({ emailChangeMessage: res.message })
+      }
+    })
+  }
+
+  handleChangeEmailClick = () => {
+    this.setState({ isChangingEmail: !this.state.isChangingEmail })
   }
 
   renderGeneral = () => {
@@ -28,8 +70,44 @@ class Settings extends Component {
           <div className="setting-menu-item">
             <div className="row">
               <div className="col-sm-3">Change Email</div>
-              <div className="col-sm-9">
+              {!this.state.isChangingEmail && (
+                <div className="col-sm-9">
+                  <button onClick={this.handleChangeEmailClick.bind(this)}>
+                    Edit
+                  </button>
+                </div>
+              )}
+              <div
+                className={`col-sm-9 accordion ${this.state.isChangingEmail &&
+                  "open"}`}
+              >
                 You can change your email address to new one
+                <form onSubmit={this.onSaveEmailChange}>
+                  <label className="label-style control-label">Old Email</label>
+                  <input
+                    onChange={this.onChangeInput}
+                    type="oldEmail"
+                    name="oldEmail"
+                    className="form-control"
+                  />
+                  <label className="label-style control-label">New Email</label>
+                  <input
+                    onChange={this.onChangeInput}
+                    type="newEmail"
+                    name="newEmail"
+                    className="form-control"
+                  />
+                  <p>{this.state.emailChangeMessage}</p>
+                  <button type="submit" onClick={this.onSaveEmailChange}>
+                    Save Changes
+                  </button>
+                  <button
+                    type="button"
+                    onClick={this.handleChangeEmailClick.bind(this)}
+                  >
+                    Cancel
+                  </button>
+                </form>
               </div>
             </div>
           </div>
