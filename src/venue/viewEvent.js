657864fe61480cbff081a4d75e35658998f0581e
robbieve <robbieeve751994@gmail.com>
2018-04-08 03:18:19 +0800

    Add an image space in the view event modal instead of a background.

--- src/venue/viewEvent.js
+++ src/venue/viewEvent.js
@@ -5,7 +5,6 @@ class ViewEvent extends Component {
     super()
     this.state = {
       isLoading: false,
-      openManageEvent: true,
       event: {},
       eventStaffs: [2, 4, 5, 7]
     }
@@ -27,13 +26,7 @@ class ViewEvent extends Component {
           </span>
           <div className="row">
             <div className="col-sm-5">
-              <div
-                className="header-event-detail-modal"
-                style={{
-                  backgroundImage: `url(${require(".././assets/Background1.png")})`,
-                  backgroundSize: "cover"
-                }}
-              >
+              <div className="header">
                 <p style={{ fontSize: "20px", fontWeight: "bolder" }}>
                   Staff Meeting for Lumi Bar
                 </p>
@@ -42,59 +35,45 @@ class ViewEvent extends Component {
                 </p>
                 <p>Event Starts at 06:30 PM </p>
               </div>
+              <div style={{ height: "auto", width: "100%" }}>
+                <img
+                  style={{ height: "inherit", width: "inherit" }}
+                  src={require(".././assets/Background1.png")}
+                />
+              </div>
               <div className="sem">
-                <div>
-                  <p>
-                    The run will begin at 9:00 A.M. Participants must be
-                    registered by this point. We are expecting to have around
-                    100 participants. We will have sent registration
-                    applications out prior to the event, to smaller running
-                    clubs in the area. Registration fees are $20. The fees are
-                    to help cover park permit fees, park participant fees,
-                    parking fees, and refreshments. If a participant has
-                    registered early they will have a parking pass distributed
-                    by us and the East Bay Regional Parks will be reimbursed by
-                    the end of the event. If a participant has not registered
-                    early they will not have a parking pass and will have to pay
-                    for parking on their own. The run/walk will go 2 ½ miles in
-                    one direction then turn around and take the same route back.
-                    It will begin at the Macdonald Staging area. Participants
-                    will run from there to Bort Meadow Staging area and back. We
-                    will set up two water stations on the trail. One at Bort
-                    Meadow Staging area and one halfway in between Bort Meadow
-                    Staging area and Macdonald Staging area. Our water stations
-                    will be operated by volunteers. We will be requesting a
-                    vehicle access permit to set up the water station that will
-                    be between the two staging areas.
-                  </p>
-                </div>
-                <div className="row">
-                  <div className="col-sm-6">
-                    <p>Date</p>
-                    <p style={{ fontWeight: "bolder" }}>April 5, 2016</p>
-                  </div>
-                  <div className="col-sm-6">
-                    <p>Time</p>
-                    <p style={{ fontWeight: "bolder" }}>9:00 PM</p>
-                  </div>
+                <div className="v-scroll scroll view-event-modal-description">
+                  The run will begin at 9:00 A.M. Participants must be
+                  registered by this point. We are expecting to have around 100
+                  participants. We will have sent registration applications out
+                  prior to the event, to smaller running clubs in the area.
+                  Registration fees are $20. The fees are to help cover park
+                  permit fees, park participant fees, parking fees, and
+                  refreshments. If a participant has registered early they will
+                  have a parking pass distributed by us and the East Bay
+                  Regional Parks will be reimbursed by the end of the event. If
+                  a participant has not registered early they will not have a
+                  parking pass and will have to pay for parking on their own.
+                  The run/walk will go 2 ½ miles in one direction then turn
+                  around and take the same route back. It will begin at the
+                  Macdonald Staging area. Participants will run from there to
+                  Bort Meadow Staging area and back. We will set up two water
+                  stations on the trail. One at Bort Meadow Staging area and one
+                  halfway in between Bort Meadow Staging area and Macdonald
+                  Staging area. Our water stations will be operated by
+                  volunteers. We will be requesting a vehicle access permit to
+                  set up the water station that will be between the two staging
+                  areas.
                 </div>
               </div>
             </div>
             <div className="col-sm-7">
               <div className="header">
-                <p>YOUR STAFF FOR THIS EVENT</p>
-                <p>
-                  EVENT FOR APRIL 5 ( <span>TODAY</span> )
+                <p style={{ fontSize: "20px", fontWeight: "bolder" }}>
+                  Staff for this event
                 </p>
               </div>
-              <div className="search">
-                <i className="fa fa-search" />
-                <input
-                  className="a-plain-text"
-                  placeholder="Enter name to find staff"
-                />
-              </div>
-              <div className="staff-list v-scroll scroll">
+              <div className="staff-list v-scroll scroll view-event-modal-description-staff-list">
                 <div className="row">
                   {this.state.eventStaffs.map(staff => {
                     return this.renderStaffBox(false, "col-sm-3", staff)
