1ff5377e05219159ad778a5e7870fed5eef34aaf
robbieve <robbieeve751994@gmail.com>
2018-04-16 14:10:01 +0800

    Subscription > Modal > DOM

--- src/SubscriptionSettings/SubscriptionSettings.js
+++ src/SubscriptionSettings/SubscriptionSettings.js
@@ -7,51 +7,88 @@ import "./SubscribeSettings.css"
 export default class SubscribeSettings extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      myProfile: {
+        showSubscriptionSettingPopup: true
+      }
+    }
+  }
+  modal() {
+    return (
+      <div className="a-modal show">
+        <div className="a-modal-content">
+          <h4>Your Subscriptions</h4>
+          <div className="row">
+            <label className="col-md-6">Attender Premium</label>
+            <div className="col-md-6 pull-right">
+              <span>$49/mo</span>
+              <br />
+              <sub>One month of Service</sub>
+            </div>
+          </div>
+          <div className="row">
+            <small className="pull-left">Purchased September 9 2017</small>
+            <small className="pull-right">Expires on October 10 2017</small>
+          </div>
+          <hr />
+          <em>
+            Note : Subscriptions are renewed on a month by month basis until
+            cancelled.
+          </em>
+          <div className="a-modal-footer">
+            <Button>Cancel Subscription</Button>
+          </div>
+        </div>
+      </div>
+    )
   }
   render() {
     return (
       <div>
+        {this.state.myProfile.showSubscriptionSettingPopup
+          ? this.modal()
+          : null}
         <NavBar />
         <div className="container lem">
           <div className="container subscribe-settings">
             <h4>Your Subscription</h4>
             <ul>
               <li>
-                <Link to="/">
-                  Attender Premium <i class="fa fa-angle-right" />
-                </Link>
+                <a onClick={this.modal}>
+                  Attender Premium <i className="fa fa-angle-right" />
+                </a>
               </li>
             </ul>
             <h3>Managing Staff</h3>
             <ul>
               <li>
                 <Link to="/">
-                  Andrew Orsen <i class="fa fa-angle-right" />
+                  Andrew Orsen <i className="fa fa-angle-right" />
                 </Link>
               </li>
               <li>
                 <Link to="/">
-                  Bea Alonzo <i class="fa fa-angle-right" />
+                  Bea Alonzo <i className="fa fa-angle-right" />
                 </Link>
               </li>
               <li>
                 <Link to="/">
-                  Pia Wurztbach <i class="fa fa-angle-right" />
+                  Pia Wurztbach <i className="fa fa-angle-right" />
                 </Link>
               </li>
               <li>
                 <Link to="/">
-                  Angel Locsin <i class="fa fa-angle-right" />
+                  Angel Locsin <i className="fa fa-angle-right" />
                 </Link>
               </li>
               <li>
                 <Link to="/">
-                  Andrew Orsen <i class="fa fa-angle-right" />
+                  Andrew Orsen <i className="fa fa-angle-right" />
                 </Link>
               </li>
               <li>
                 <Link to="/">
-                  Bea Alonzo <i class="fa fa-angle-right" />
+                  Bea Alonzo <i className="fa fa-angle-right" />
                 </Link>
               </li>
             </ul>
