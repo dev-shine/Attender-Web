233c75f3e61c86efef517c480bc2835d3d5ad7a3
robbieve <robbieeve751994@gmail.com>
2018-05-23 15:36:15 +0800

    Enable the staff time pickers.

--- src/components/venue/staffTimePicker.js
+++ src/components/venue/staffTimePicker.js
@@ -1,9 +1,9 @@
 import React, { PureComponent } from "react"
-// import MomentUtils from "material-ui-pickers/utils/moment-utils"
-// import MuiPickersUtilsProvider from "material-ui-pickers/utils/MuiPickersUtilsProvider"
-// import TimePicker from "material-ui-pickers/TimePicker"
-// import DatePicker from "material-ui-pickers/DatePicker"
-// import DateTimePicker from "material-ui-pickers/DateTimePicker"
+import MomentUtils from "material-ui-pickers/utils/moment-utils"
+import MuiPickersUtilsProvider from "material-ui-pickers/utils/MuiPickersUtilsProvider"
+import TimePicker from "material-ui-pickers/TimePicker"
+import DatePicker from "material-ui-pickers/DatePicker"
+import DateTimePicker from "material-ui-pickers/DateTimePicker"
 
 export default class StaffTimePicker extends PureComponent {
   constructor(props) {
@@ -12,13 +12,12 @@ export default class StaffTimePicker extends PureComponent {
 
   render() {
     return (
-      <div>test</div>
-      // <MuiPickersUtilsProvider utils={MomentUtils}>
-      //   <TimePicker
-      //     value={this.props.selectedTime}
-      //     onChange={this.props.onSelectTime}
-      //   />
-      // </MuiPickersUtilsProvider>
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <TimePicker
+          value={this.props.selectedTime}
+          onChange={this.props.onSelectTime}
+        />
+      </MuiPickersUtilsProvider>
     )
   }
 }
