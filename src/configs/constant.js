aef02e7538d19d1271725176adc48519afa846a7
robbieve <robbieeve751994@gmail.com>
2018-06-22 14:57:40 +0800

    Fixing constants js

---
+++ src/configs/constant.js
@@ -0,0 +1,12 @@
+console.log("hostname : " + window.location.hostname)
+var config = {}
+if (window.location.hostname === "localhost") {
+  config = {
+    API_URL: "http://localhost:3333/api/"
+  }
+} else {
+  config = {
+    API_URL: "https://staging.attender.com.au/api/"
+  }
+}
+module.exports = config
