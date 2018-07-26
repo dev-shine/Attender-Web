dc48aa0e59d78121ff6050c0f18ee6929d7912a7
robbieve <robbieeve751994@gmail.com>
2018-07-27 05:17:20 +0800

    Resend signup confirmation link in email fix

--- src/components/auth/signSuccess.js
+++ src/components/auth/signSuccess.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 import API from "./../../services/api"
+import { Button } from "react-bootstrap"
 
 class SignSuccess extends Component {
   constructor(props) {
@@ -13,6 +14,9 @@ class SignSuccess extends Component {
 
   onResend = async () => {
     let response = await API.post("auth/resend", {})
+    alert(
+      "We have re-sent the confirmation link to your email. Please check your inbox (or spam) and click the link to confirm your account."
+    )
   }
 
   render() {
@@ -30,12 +34,12 @@ class SignSuccess extends Component {
               Couldn't find your confirmation email in your inbox or spam/junk?
               Click below to resend.
             </p>
-            <button
+            <Button
               onClick={() => this.onResend()}
               className="a-btn btn-round btn-active wide-xm"
             >
               Resend Confirmation
-            </button>
+            </Button>
           </div>
         </div>
       </div>
