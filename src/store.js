012013ec7325415ad182ba32c560492905490528
robbieve <robbieeve751994@gmail.com>
2018-08-09 14:23:42 +0800

    messagin - add static missing section and styles

--- src/store.js
+++ src/store.js
@@ -35,6 +35,9 @@ if (process.env.NODE_ENV === "development") {
   }
 }
 
-const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers)
+const composedEnhancers = compose(
+  applyMiddleware(...middleware),
+  ...enhancers
+)
 
 export default createStore(rootReducer, initialState, composedEnhancers)
