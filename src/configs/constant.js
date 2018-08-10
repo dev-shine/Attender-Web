f059163fc6b970f2607cf5514c194d05a275a965
robbieve <robbieeve751994@gmail.com>
2018-08-10 09:28:53 +0800

    Revert "listing archive price"
    This reverts commit dd89a5e58614c122077b3d6c954cd74d4960f1cf.

--- src/configs/constant.js
+++ src/configs/constant.js
@@ -1,7 +1,7 @@
 var config = {}
 if (window.location.hostname === "localhost") {
   config = {
-    API_URL: "https://staging.attender.com.au/api/"
+    API_URL: "http://localhost:3333/api/"
   }
 } else {
   config = {
